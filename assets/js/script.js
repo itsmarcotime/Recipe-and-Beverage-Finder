7d1a07c3b842d3627faf89271f1eacc93a622d56
panecatl <ypanecatl24@gmail.com>
2022-06-12 13:34:37 -0500

    js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,71 @@
-var input = document.querySelector("search-input");
-var searchBtn = document.querySelector("search-btn");
-var recipeResult = document.querySelector("recipe-result");
+let recipeResult = document.querySelector("recipe-result");
+let searchBtn = document.querySelector("search-btn");
+let apiUrl = "https:////www.themealdb.com/api/json/v1/1/search.php?s=";
 
-var url = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + input;
+searchBtn.addEventListener("click", () => {
+    let userInp = document.querySelector("search-input").ariaValueMax;
 
-var getApi = function() {
-    fetch (url).then ((response) => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            alert("Please enter a valid food type.");
-        }
+    // incase user searches invalid recipe or null
+    if (userInp.length === 0) {
+        result.innerHTML = "<h3>Please Enter a valid recipe search.</h3>";
+    } else {
+        fetch(apiUrl + userInp).then ((response) => response.json())
+        .then((data) => {
+            let myMeal = data.meal[0];
+            console.log(myMeal);
+            console.log(myMeal.strMealThumb);
+            console.log(myMeal.strMeal);
+            console.log(myMeal.strArea);
+            console.log(myMeal.strInstructions);
+            let count = 1;
+            let ingredients = [];
+
+            for (let i in myMeal) {
+                let ingredient = "";
+                let measure = "";
+                if (i.startsWith("strIngredient") && myMeal[i]) {
+                    ingredient = myMeal[i];
+                    measure = myMeal["strMeasur" + count];
+                    count += 1;
+                    ingredients.push(`${measure} ${ingredient}`);
+                }
+            }
+            console.log(ingredients);
+
+            result.innerHtml = `
+            <img src=${myMeal.strMealThumb}>
+            <div class="details">
+                <h2>${myMeal.strMeal}</h2>
+                <h4>${myMeal.strArea}</h4>
+            </div>
+            <div id="ingredient-con"></div>
+            <div id="recipe">
+                <button id="hide-recipe">X</button>
+                <pre id="instructions">${myMeal.strInstructions}</pre>
+            </div>
+            <button id="show-recipe">View Recipe</button>
+            `;
+
+            let ingredientCon = document.getElementById("ingredient-con");
+            let parent = document.createElement("ul");
+            let recipe = document.getElementById("recipe");
+            let hideRecipe = document.getElementById("hide-recipe");
+            let showRecipe = document.getElementById("show-recipe");
+            ingredients.forEach((i) => {
+                let child = document.createElement("li");
+                child.innerText = i;
+                parent.appendChild(child);
+                ingredientCon.appendChild(parent);
+            });
+            hideRecipe.addEventListener("click", () => {
+                recipe.style.display = "none";
+            });
+            showRecipe.addEventListener("click", () => {
+                recipe.style.display = "block";
+            });
     })
-    .then(data => {
-        console.log(data);
-    })
-}
+    .catch(() => {
+        result.innerHTML = `<h3>Invalid Input</h3>`;
+    });
+    }
+})
