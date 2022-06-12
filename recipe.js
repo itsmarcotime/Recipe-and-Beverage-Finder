6cb30fe56211645fc12b181536dcd31350cfc9c0
panecatl <ypanecatl24@gmail.com>
2022-06-12 12:01:00 -0500

    updating html and js

--- recipe.js
+++ recipe.js
@@ -1,14 +1,36 @@
 var input = document.querySelector("search-input");
 var searchBtn = document.querySelector("search-btn");
-var recipeResult = document.querySelector("recipe-resyult");
+var recipeResult = document.querySelector("recipe-result");
 
 var url = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + input;
 
-function getApit() {
-fetch(url)
-.then((response) => response.json())
-.then((data) => {
-    console.log(data);
-});
-};
-searchButton.addEventListener("click", fetch(url));
+var getApi = function() {
+    fetch (url).then ((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            alert("Please enter a valid food type.");
+        }
+    })
+    .then(data => {
+        console.log(data);
+    })
+}
+
+
+
+
+
+
+
+
+
+
+// function getApit() {
+// fetch(url)
+// .then((response) => response.json())
+// .then((data) => {
+//     console.log(data);
+// });
+// };
+// searchButton.addEventListener("click", fetch(url));
