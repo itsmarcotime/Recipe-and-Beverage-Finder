2c600224298acdec77e55245b861b0954e1f2d23
panecatl <ypanecatl24@gmail.com>
2022-06-12 19:10:34 -0500

    workig js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,14 @@ var searchBtn = document.querySelector("search-btn");
 var result = document.querySelector("recipe-result"); 
 var recipeContainerEl = document.querySelector("recipe-container");
 
-function getRecipe() {
-    var requestUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s="
-    fetch (requestUrl).then(function(response) {
-        return response.json();
-        console.log(requestUrl);  
-    })
-};
+var getRecipe = function(recipe) {
+    var apiUrl = ("https://www.themealdb.com/api/json/v1/1/search.php?s=" + recipe)
 
-getRecipe();
+    // make request to api
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+getRecipe("big mac");
