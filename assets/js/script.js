6bfe11d2636c6cd5a33964ed392f789fabe0855b
panecatl <ypanecatl24@gmail.com>
2022-06-12 20:01:55 -0500

    able to search for recipe in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-var search = document.querySelector("search-input");
+var userFormEl = document.querySelector("#user-form");
+var searchFormEl = document.querySelector("#search-form");
+
 var searchBtn = document.querySelector("search-btn");
 var result = document.querySelector("recipe-result"); 
 var recipeContainerEl = document.querySelector("recipe-container");
@@ -13,4 +15,19 @@ var getRecipe = function(recipe) {
         });
     });
 };
-getRecipe("big mac");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    // getting value from input element 
+    var searchedRecipe = searchFormEl.value.trim();
+
+    if (searchedRecipe) {
+        getRecipe(searchedRecipe);
+        searchFormEl.value="";
+    } else {
+        alert("Please enter a correct recipe search.")
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
