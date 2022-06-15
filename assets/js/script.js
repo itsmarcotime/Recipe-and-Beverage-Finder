0997b12d21989322ae5ed4646a532553d486acee
Zchun <z.c.chun2003@gmail.com>
2022-06-15 14:51:06 -0500

    added mealStore option

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var searchFormEl = document.querySelector("#search-form");
 var randBtnEl = document.querySelector('#rand-btn');
 var recipeContainerEl = document.querySelector("#recipe-container");
 var result = document.querySelector("#recipe-result"); 
+var mealIngredientEl = document.querySelector('#meal-ingredients');
+var mealInstructionsEl = document.querySelector('#meal-instruct');
 var drinkFormEl = document.querySelector('#drink-srch');
 var drinkContainerEl = document.querySelector('#drink-container');
 var drinkIngredientEl = document.querySelector('#drink-ingredients');
@@ -46,9 +48,11 @@ var displayMeal = function(recipe, searchTerm) {
     // check to see if there are any recipes
     console.log(recipe);
     console.log(searchTerm);
+    mealIngredientEl.textContent = "";
 
 
-let myMeal = recipe.meals[0]; 
+let myMeal = recipe.meals[0];
+let instruct = myMeal.strInstructions; 
 let count = 1;
 let ingredients = [];
 for (let i in myMeal) {
@@ -58,29 +62,21 @@ for (let i in myMeal) {
     ingredient = myMeal[i];
     measure = myMeal["strMeasure" + count];
     count += 1;
-    ingredients.push(`${measure} ${ingredient}`); 
-                }
-            }
+    ingredients.push(`${measure} ${ingredient},`); 
+    }
+}
 
-    recipeContainerEl.textContent = "";
-    result.textContent = myMeal.strMeal; 
-    // probable for loop to go through all of the different meals 
-    //for (var i = 0; i <recipe.meals.length; i++) {
-        // format recipe name with ingredients/measurements, and intructions
-        var recipeName = recipe.meals[0].strArea + " " + myMeal.strInstructions + " " + myMeal.strMeasure + " " + recipe.meals[0].strInstructions;
+result.textContent = myMeal.strMeal; 
+    for (let i = 0; i < ingredients.length; i++){
+        let ingredientEl = document.createElement("li");
+        ingredientEl.textContent = ingredients[i];
+        console.log(ingredients[i]);
+        mealIngredientEl.appendChild(ingredientEl);
+    }
 
-        // create container for recipe
-        var recipeEl = document.createElement("div");
-        recipeEl.classList = "list-item flex-row justify-space-between align-center";
+    mealInstructionsEl.textContent = instruct;
 
-        // create span to hold recipe
-        var titleEl = document.createElement("span");
-        titleEl.textContent = recipeName;
-
-        // appending
-        recipeEl.appendChild(titleEl);
-
-        recipeContainerEl.appendChild(recipeEl);
+    mealSaveEl.addEventListener("click", mealStore);
     } 
 //}
 
@@ -167,6 +163,27 @@ let drinkStore = function(event) {
     listPrint();
 }
 
+let mealStore = function(event) {
+    event.preventDefault();
+
+
+    //check local storage if clear set to an empty array
+    let shoppingList = JSON.parse(localStorage.getItem("shoppingList")) || [];
+
+    let mealInfo = mealIngredientEl.textContent;
+    let ingredient = mealInfo.split(",");
+    let recipe = {name: result.textContent, list: ingredient};
+    for (let i = 0; i < (ingredient.length) - 1; i++) {
+        console.log(ingredient[i]);
+    }
+    console.log(recipe);
+
+    shoppingList.push(recipe);
+    localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+
+    listPrint();
+}
+
 let listPrint = function() {
 
     groceryListEl.textContent = ""
@@ -175,8 +192,21 @@ let listPrint = function() {
     console.log(shoppingList);
     for (let i = 0; i < shoppingList.length; i++){
         let itemName = document.createElement("li");
+        itemName.classList.add("grocery-name");
         itemName.textContent = shoppingList[i].name;
         groceryListEl.appendChild(itemName);
+
+        //create a list element to store ingredient list
+        let ingredList = document.createElement("ul");
+        ingredList.id = shoppingList[i].name;
+        ingredList.classList.add("ingredient-name");
+        itemName.appendChild(ingredList);
+           
+        for (let j in shoppingList[i].list) {
+            let ingredientEl = document.createElement("li");
+            ingredientEl.textContent = shoppingList[i].list[j];
+            ingredList.appendChild(ingredientEl);
+        }
     }
 }
 
