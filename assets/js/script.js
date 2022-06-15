6fa52de9712f2d9e6172c34c8a72884ac312360a
Zchun <z.c.chun2003@gmail.com>
2022-06-15 14:06:19 -0500

    added drink storage display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var userFormEl = document.querySelector("#user-form");
 var searchFormEl = document.querySelector("#search-form");
+var randBtnEl = document.querySelector('#rand-btn');
 var recipeContainerEl = document.querySelector("#recipe-container");
 var result = document.querySelector("#recipe-result"); 
 var drinkFormEl = document.querySelector('#drink-srch');
@@ -10,6 +11,7 @@ var drinkInstructionsEl = document.querySelector('#drink-instruct');
 var searchBtn = document.querySelector("#search-btn");
 var mealSaveEl = document.querySelector('#meal-save');
 var drinkSaveEl = document.querySelector('#drink-save');
+var groceryListEl = document.querySelector('#grocery-list');
 
 
 
@@ -122,7 +124,7 @@ let displayDrink = function(drink) {
             ingredient = drink[i];
             measure = drink["strMeasure" + count];
             count += 1;
-            ingredients.push(`${measure} ${ingredient}`); 
+            ingredients.push(`${measure} ${ingredient},`); 
         }
     }
     for (let i = 0; i < ingredients.length; i++){
@@ -140,15 +142,45 @@ let displayDrink = function(drink) {
     console.log(instruct);
     console.log(ingredients);
     console.log(isAlcoholic);
+
+    drinkSaveEl.addEventListener("click", drinkStore);
 }
 
-let mealStore = function(event) {
+let drinkStore = function(event) {
     event.preventDefault();
 
-    let drinkInfo = IngredientEl.textContent;
-    console.log(drinkInfo);
+
+    //check local storage if clear set to an empty array
+    let shoppingList = JSON.parse(localStorage.getItem("shoppingList")) || [];
+
+    let drinkInfo = drinkIngredientEl.textContent;
+    let ingredient = drinkInfo.split(",");
+    let recipe = {name: drinkResult.textContent, list: ingredient};
+    for (let i = 0; i < (ingredient.length) - 1; i++) {
+        console.log(ingredient[i]);
+    }
+    console.log(recipe);
+
+    shoppingList.push(recipe);
+    localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+
+    listPrint();
 }
 
-drinkSaveEl.addEventListener("click", mealStore);
+let listPrint = function() {
+
+    groceryListEl.textContent = ""
+
+    let shoppingList = JSON.parse(localStorage.getItem("shoppingList")) || [];
+    console.log(shoppingList);
+    for (let i = 0; i < shoppingList.length; i++){
+        let itemName = document.createElement("li");
+        itemName.textContent = shoppingList[i].name;
+        groceryListEl.appendChild(itemName);
+    }
+}
+
+
 userFormEl.addEventListener("submit", formSubmitHandler);
-userFormEl.addEventListener("click", cocktailRand);
+randBtnEl.addEventListener("click", cocktailRand);
+listPrint();
